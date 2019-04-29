f1312f1211e35ce6a6932e77b925570c57bb599a
irek02 <imirgaleev@gmail.com>
2019-04-29 19:55:11 -0400

    remove routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './src/components/header/header.component';
 import { HomesComponent } from './src/components/homes/homes.component';
@@ -15,7 +14,6 @@ import { HomesComponent } from './src/components/homes/homes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
