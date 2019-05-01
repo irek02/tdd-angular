59c7e75dd707d91fd602f4ba1592fd2c5a3a92ca
irek02 <imirgaleev@gmail.com>
2019-04-30 20:24:26 -0400

    remove tests from app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,2 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './src/components/header/header.component';
-import { HomesComponent } from './src/components/homes/homes.component';
-
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-      ],
-      declarations: [
-        AppComponent,
-        HeaderComponent,
-        HomesComponent
-      ],
-    }).compileComponents();
-  }));
-
 });
