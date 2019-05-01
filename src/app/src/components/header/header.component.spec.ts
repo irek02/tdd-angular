4efe5ba8570edf365a79e1bb5d35f8f22c7bf584
irek02 <imirgaleev@gmail.com>
2019-05-01 07:24:13 -0400

    made the tests pass; next style

--- src/app/src/components/header/header.component.spec.ts
+++ src/app/src/components/header/header.component.spec.ts
@@ -24,4 +24,27 @@ describe('HeaderComponent', () => {
     expect(fixture.nativeElement.querySelector('[data-test="logo"]')).toBeTruthy();
 
   });
+
+  it('should show search', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="search"]')).toBeTruthy();
+
+  });
+
+  it('should show menu', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="menu"]')).toBeTruthy();
+
+  });
+
+  it('should show filters', () => {
+
+    expect(fixture.nativeElement.querySelector('[data-test="home-type"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="dates"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="guests"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="price"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="rooms"]')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('[data-test="amenities"]')).toBeTruthy();
+
+  });
 });
